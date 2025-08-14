19f1baad836aa714197091a1f8f7ad351ec93f67
LebedevaYS <yulia.03.l@gmail.com>
2025-08-14 15:04:59 +0300

    Добавила позитивный и негативный набор тестовых данных в тестирование авторизации

--- src/test/java/com/example/LoginHerokuTestNG.java
+++ src/test/java/com/example/LoginHerokuTestNG.java
@@ -2,6 +2,7 @@ package com.example;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.*;
@@ -15,27 +16,39 @@ public class LoginHerokuTestNG {
     private LoginHerokuPage loginPage;
     private ProfileHerokuPage profilePage;
 
+    @DataProvider(name = "logins")
+    public Object[][] loginData() {
+        return new Object[][] {
+                //{ConfProperties.getProperty("rightlogin"), ConfProperties.getProperty("rightpassword"), "You logged into a secure area!"},
+                {ConfProperties.getProperty("wronglogin"), ConfProperties.getProperty("wrongpassword"), "Your username is invalid!"}
+        };
+    }
 
-    @BeforeClass
+    @BeforeMethod
     public void setup() {
         System.setProperty("webdriver.chrome.driver", ConfProperties.getProperty("chromedriver"));
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.setAcceptInsecureCerts(true);
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
-        driver.get("https://the-internet.herokuapp.com/login");
         loginPage = new LoginHerokuPage(driver);
         profilePage = new ProfileHerokuPage(driver);
+        driver.get("https://the-internet.herokuapp.com/login");
     }
 
-    @Test
-    public void loginTest() {
-        loginPage.inputLogin("tomsmith");
-        loginPage.inputPassword("SuperSecretPassword!");
+
+    @Test(dataProvider = "logins")
+    public void loginTest(String username, String password, String expectedMessage) {
+        loginPage.inputLogin(username);
+        loginPage.inputPassword(password);
         loginPage.clickLoginBtn();
 
-        String message = profilePage.getFlashMessage();
-        Assert.assertTrue(message.contains("You logged into a secure area!"), "Login failed!");
+        String actualMessage = profilePage.getFlashMessage();
+        System.out.print("message:" + actualMessage);
+        Assert.assertTrue(actualMessage.contains(expectedMessage),
+                "Login result does not match expected message!");
     }
 
     @AfterClass
